d3b02b92037f9b409dbda0107b946b4986d78103
jonasbrocas007 <55618670+jonasbrocas007@users.noreply.github.com>
2022-07-04 23:30:51 +0100

    Best code ever

---
+++ chad.py
@@ -0,0 +1,27 @@
+import pygame
+import os
+from pygame import mixer
+pygame.display.init()
+pygame.font.init()
+textfont = pygame.font.SysFont("monospace",16)
+file = 'chad.jpg'
+render = input(str("O que queres:"))
+render2 = input(str("O que queres:"))
+
+screen = pygame.display.set_mode((640, 480))
+load = pygame.image.load(file)
+screen.blit(load,(0,0))
+
+textTBD = textfont.render(render, 1, (0,0,0))
+screen.blit(textTBD, (0,22))
+
+textTBD2 = textfont.render(render2, 1, (0,0,0))
+screen.blit(textTBD2, (322,22))
+
+mixer.init()
+music = 'musica.mp3'
+mixer.music.load(music)
+mixer.music.play()
+
+pygame.display.update
+pygame.display.flip()
